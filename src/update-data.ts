88392dce37f1ee42a0600cb780c95c0d9f3b69db
Richard Lindner <rlindner81@gmail.com>
2021-06-03 13:07:03 +0200

    more data loading

--- src/update-data.ts
+++ src/update-data.ts
@@ -3,29 +3,19 @@ import { getMarketHistory } from "./tradier.ts";
 
 type Stock = Record<"symbol", string>;
 
-const STOCKS: Array<Stock> = [
-  { symbol: "BARK" },
-  { symbol: "CRSR" },
-  { symbol: "STOR" },
-  { symbol: "BOWX" },
-  { symbol: "CCL" },
-  { symbol: "CCIV" },
-  { symbol: "PLTR" },
-  { symbol: "TWTR" },
-  { symbol: "AMD" },
-  { symbol: "DIS" },
-  // CD PROJEKT
-];
+const STOCKS_FILE = "data/stocks.json";
 
-const dataFileForSymbol = (symbol: string) => `data-${symbol}.json`;
+const dataFileForSymbol = (symbol: string) => `data/data-${symbol}.json`;
 
 const loadDataForSymbol = async (symbol: string) => {
   const file = dataFileForSymbol(symbol);
   if (await exists(file)) {
+    console.log("loading data for symbol (cached) %s", symbol);
     const dataRaw = await Deno.readFile(file);
     const dataText = new TextDecoder("utf-8").decode(dataRaw);
     return JSON.parse(dataText);
   }
+  console.log("loading data for symbol %s", symbol);
 
   const data = await getMarketHistory(symbol);
   const dataText = JSON.stringify(data, null, 2) + "\n";
@@ -38,7 +28,10 @@ const loadDataForSymbols = async (list: Array<Stock>) =>
   await Promise.all(list.map(({ symbol }) => loadDataForSymbol(symbol)));
 
 const main = async () => {
-  await loadDataForSymbols(STOCKS);
+  const stocksRaw = await Deno.readFile(STOCKS_FILE);
+  const stocksText = new TextDecoder("utf-8").decode(stocksRaw);
+  const stocks: Array<Stock> = JSON.parse(stocksText);
+  await loadDataForSymbols(stocks);
   const i = 0;
 };
 
