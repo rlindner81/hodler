9c1de92bbc5bed8e986313b708384c6f2261fe83
Richard Lindner <rlindner81@gmail.com>
2021-06-13 19:21:48 +0200

    options-quotes

--- src/tradier.ts
+++ src/tradier.ts
@@ -1,6 +1,17 @@
 const TRADIER_APIKEY_ENV_VAR = "TRADIER_APIKEY";
 const TRADIER_HOST = new URL("https://sandbox.tradier.com/");
 
+interface Quote {
+  symbol: string,
+  bid: number,
+  ask: number,
+  open: number,
+  high: number,
+  low: number,
+  close: number
+}
+type Quotes = Record<"quotes", Record<"quote", Array<Quote>>>;
+
 const getMarketHistory = async (symbol: string) => {
   const nowDate = new Date();
   const start = `${nowDate.getFullYear()}-01-01`;
@@ -19,9 +30,9 @@ const getMarketHistory = async (symbol: string) => {
   return reponse.json();
 };
 
-const getMarketQuotes = async (symbols: string) => {
+const getMarketQuotes = async (symbols: Array<string>): Promise<Quotes> => {
   const tradierUrl = new URL("/v1/markets/quotes", TRADIER_HOST);
-  tradierUrl.search = new URLSearchParams({ symbols }).toString();
+  tradierUrl.search = new URLSearchParams({ symbols: symbols.join(",") }).toString();
   const headers = {
     "Accept": "application/json",
     "Authorization": `Bearer ${Deno.env.get(TRADIER_APIKEY_ENV_VAR)}`,
