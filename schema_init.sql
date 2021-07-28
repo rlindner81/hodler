3fda2ce4a70d374ba8dc71b598f0f4df14a6a8f4
Richard Lindner <rlindner81@gmail.com>
2021-07-29 00:13:26 +0200

    more schema work 2

--- schema_init.sql
+++ schema_init.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS t_user;
-DROP TABLE IF EXISTS t_broker;
-DROP TABLE IF EXISTS t_currency;
-DROP TABLE IF EXISTS t_security_type;
-DROP TABLE IF EXISTS t_security;
-DROP TABLE IF EXISTS t_cash_transaction_type;
-DROP TABLE IF EXISTS t_cash_transaction;
-DROP TABLE IF EXISTS t_security_transaction_type;
-DROP TABLE IF EXISTS t_security_transaction;
+DROP TABLE IF EXISTS t_user CASCADE;
+DROP TABLE IF EXISTS t_broker CASCADE;
+DROP TABLE IF EXISTS t_currency CASCADE;
+DROP TABLE IF EXISTS t_security_type CASCADE;
+DROP TABLE IF EXISTS t_security CASCADE;
+DROP TABLE IF EXISTS t_security_transaction_type CASCADE;
+DROP TABLE IF EXISTS t_security_transaction CASCADE;
+DROP TABLE IF EXISTS t_cash_transaction_type CASCADE;
+DROP TABLE IF EXISTS t_cash_transaction CASCADE;
 
 CREATE TABLE IF NOT EXISTS t_user
 (
     identifier VARCHAR(256) PRIMARY KEY NOT NULL,
     name       VARCHAR(256)             NOT NULL
 );
-INSERT INTO t_currency (identifier, name)
+INSERT INTO t_user (identifier, name)
 VALUES ('rlindner81@gmail.com', 'Richard')
 ;
 
@@ -55,8 +55,33 @@ CREATE TABLE IF NOT EXISTS t_security
     underlying VARCHAR(256) REFERENCES t_security (identifier)      NOT NULL
 );
 INSERT INTO t_security (identifier, currency, type, underlying)
-VALUES ('AMD220121C00077500', 'USD', 'OPTION', 'AMD')
+VALUES ('AMD', 'USD', 'STOCK', 'AMD'),
+       ('AMD220121C00077500', 'USD', 'OPTION', 'AMD')
 ;
+
+CREATE TABLE IF NOT EXISTS t_security_transaction_type
+(
+    identifier VARCHAR(256) PRIMARY KEY NOT NULL
+);
+INSERT INTO t_security_transaction_type (identifier)
+VALUES ('STOCK_BUY'),
+       ('STOCK_SELL'),
+       ('OPTION_BUY'),
+       ('OPTION_SELL')
+;
+CREATE TABLE IF NOT EXISTS t_security_transaction
+(
+    id            UUID PRIMARY KEY                                                 NOT NULL,
+    timestamp     TIMESTAMP WITHOUT TIME ZONE                                      NOT NULL,
+    broker        VARCHAR(256) REFERENCES t_broker (identifier)                    NOT NULL,
+    "user"        VARCHAR(256) REFERENCES t_user (identifier)                      NOT NULL,
+    type          VARCHAR(256) REFERENCES t_security_transaction_type (identifier) NOT NULL,
+    amount        INTEGER                                                          NOT NULL,
+    price         NUMERIC(20, 10)                                                  NOT NULL,
+    book_currency VARCHAR(256) REFERENCES t_currency (identifier)                  NOT NULL,
+    book_fx_rate  NUMERIC(20, 10)                                                  NOT NULL
+);
+
 
 CREATE TABLE IF NOT EXISTS t_cash_transaction_type
 (
@@ -81,28 +106,5 @@ CREATE TABLE IF NOT EXISTS t_cash_transaction
     "user"               VARCHAR(256) REFERENCES t_user (identifier)                  NOT NULL,
     type                 VARCHAR(256) REFERENCES t_cash_transaction_type (identifier) NOT NULL,
     price                NUMERIC(20, 10)                                              NOT NULL,
-    security_transaction VARCHAR(256) REFERENCES t_security_transaction (id)
-);
-
-CREATE TABLE IF NOT EXISTS t_security_transaction_type
-(
-    identifier VARCHAR(256) PRIMARY KEY NOT NULL
-);
-INSERT INTO t_security_transaction_type (identifier)
-VALUES ('STOCK_BUY'),
-       ('STOCK_SELL'),
-       ('OPTION_BUY'),
-       ('OPTION_SELL')
-;
-CREATE TABLE IF NOT EXISTS t_security_transaction
-(
-    id            UUID PRIMARY KEY                                                 NOT NULL,
-    timestamp     TIMESTAMP WITHOUT TIME ZONE                                      NOT NULL,
-    broker        VARCHAR(256) REFERENCES t_broker (identifier)                    NOT NULL,
-    "user"        VARCHAR(256) REFERENCES t_user (identifier)                      NOT NULL,
-    type          VARCHAR(256) REFERENCES t_security_transaction_type (identifier) NOT NULL,
-    amount        INTEGER                                                          NOT NULL,
-    price         NUMERIC(20, 10)                                                  NOT NULL,
-    book_currency VARCHAR(256) REFERENCES t_currency (identifier)                  NOT NULL,
-    book_fx_rate  NUMERIC(20, 10)                                                  NOT NULL
+    security_transaction UUID REFERENCES t_security_transaction (id)
 );
