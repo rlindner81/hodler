4e963ebcbb491353569307af4bc30aaf9731442d
Richard Lindner <rlindner81@gmail.com>
2021-08-04 19:58:52 +0200

    the style wars

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="nav">
-    <router-link to="/">Overview</router-link> |
-    <router-link to="/profit">Profit & Loss</router-link> |
-    <router-link to="/plays">Play Theses</router-link> |
-    <router-link to="/holdings">Holdings</router-link> |
+    <router-link to="/">Overview</router-link>
+    |
+    <router-link to="/profit">Profit & Loss</router-link>
+    |
+    <router-link to="/plays">Play Theses</router-link>
+    |
+    <router-link to="/holdings">Holdings</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view/>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Lato');
+@import url("https://fonts.googleapis.com/css?family=Lato");
 
 #app * {
-  font-family: 'Lato', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Lato", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  //color: black;
+  color: black;
 }
 
 #nav {
-  /*text-align: right;*/
-  padding: 1em 3em;
+  padding: 1em;
 
   a {
+    padding: 0.2em 1em;
     font-weight: bold;
     text-decoration: none;
+    background-color: hsla(60, 45%, 42%, 0.15);
 
-    .router-link-exact-active {
-      color: #42b983;
-    }
+  }
+
+  a:hover, a.router-link-exact-active {
+    background-color: white;
   }
 }
 </style>
