3a72d2c0c42973967140e0b2926d88ec56ee6767
Richard Lindner <rlindner81@gmail.com>
2021-11-06 08:59:26 +0100

    options-analysis.ts works

--- backend/src-extra/options-analysis.ts
+++ backend/src-extra/options-analysis.ts
@@ -14,53 +14,6 @@ const SYMBOLS = [
 
 const EXPIRY_RANGE_DAYS = 35;
 const PRICE_BUFFER = 0.95;
-
-// const getMarketResults = async () => {
-//   const { quotes: { quote: results } } = await getMarketQuotes(SYMBOLS);
-//   return results;
-// };
-//
-// const getHistoricResults = async (date: Date) => {
-//   const results = [];
-//   for (const symbol of SYMBOLS) {
-//     const startDate = new Date(date);
-//     startDate.setDate(startDate.getDate() - 10);
-//     const { history } = await getMarketHistory(symbol, startDate, date);
-//     if (history === null) {
-//       continue;
-//     }
-//     const { day } = history;
-//     const result = day[day.length - 1];
-//     results.push({
-//       symbol,
-//       ...result,
-//     });
-//   }
-//   return results;
-// };
-//
-// const readArgs = () => {
-//   const historyIndex = Deno.args.indexOf("--history");
-//   const doHistory = historyIndex !== -1;
-//   if (Deno.args.length <= historyIndex + 1) {
-//     return {
-//       doHistory,
-//       historyDate: new Date(Date.UTC(2021, 0, 1)),
-//     };
-//   }
-//
-//   const historyDateRaw = Deno.args[historyIndex + 1];
-//   const [, year, month, day] = /(\d{4})-(\d{2})-(\d{2})/.exec(historyDateRaw) ||
-//     [];
-//   const historyDate = new Date(
-//     Date.UTC(parseInt(year), parseInt(month) - 1, parseInt(day)),
-//   );
-//   return {
-//     doHistory,
-//     historyDate,
-//   };
-// };
-
 const MILLIS_IN_DAYS = 1000 * 60 * 60 * 24;
 
 const _dateDiffInDays = (a: Date, b: Date) => {
@@ -77,11 +30,11 @@ const _getLatestExpiration = (limit: number, expirations: Array<string>) => {
   return dates.length > 0 ? expirations[dates.length - 1] : null;
 }
 
-const _getHighestItmChain = (quote: any, chains: Array<any>) => {
+const _getHighestItmPutChain = (quote: any, chains: Array<any>) => {
   const price = (quote.ask + quote.bid) / 2;
   const itmPutChains = chains
     .filter(chain => chain.strike <= price * PRICE_BUFFER && chain.option_type === "put");
-  return itmPutChains[itmPutChains.length - 1];
+  return itmPutChains.length > 0 ? itmPutChains[itmPutChains.length - 1]: null;
 }
 
 const _analyzeChain = (quote: any, chain: any) => {
@@ -98,20 +51,23 @@ const _analyzeChain = (quote: any, chain: any) => {
 
 const _analyzeSymbol = async (symbol: string) => {
   // console.log("analyzing symbol %s", symbol);
-  // const [quote] = await getMarketQuotes([symbol]);
-  const [quote] = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("market-quotes-response.json")));
+  const [quote] = await getMarketQuotes([symbol]);
+  // const [quote] = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("../temp/market-quotes-response.json")));
 
-  // const expirations = await getOptionExpirations(symbol);
-  const expirations = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("option-expirations-response.json")));
+  const expirations = await getOptionExpirations(symbol);
+  // const expirations = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("../temp/option-expirations-response.json")));
   const expiration = _getLatestExpiration(EXPIRY_RANGE_DAYS, expirations);
   if (expiration === null) {
     console.warn("could not find latest expiration for %s", symbol);
     return;
   }
-  // const chains = await getOptionChains(symbol, expiration);
-  const chains = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("option-chains-response.json")));
-
-  const chain = _getHighestItmChain(quote, chains);
+  const chains = await getOptionChains(symbol, expiration);
+  // const chains = JSON.parse(new TextDecoder("utf-8").decode(await Deno.readFile("../temp/option-chains-response.json")));
+  const chain = _getHighestItmPutChain(quote, chains);
+  if (chain === null) {
+    console.warn("could not find highest itm put chain for %s", symbol);
+    return;
+  }
 
   return _analyzeChain(quote, chain);
 }
