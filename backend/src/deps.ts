a5736438d839d9e6563f642b22f3aeb9c90b3ba2
Richard Lindner <rlindner81@gmail.com>
2021-11-07 09:41:10 +0100

    deps update

--- backend/src/deps.ts
+++ backend/src/deps.ts
@@ -3,8 +3,8 @@ export {
   Context,
   Router,
   Status,
-} from "https://deno.land/x/oak@v8.0.0/mod.ts";
-export type { RouterOptions } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+} from "https://deno.land/x/oak@v9.0.1/mod.ts";
+export type { RouterOptions } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export * as validasaur from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
-export { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+export { Client } from "https://deno.land/x/postgres@v0.13.0/mod.ts";
